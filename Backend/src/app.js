dff88b01a5ba8b9d5acfe0dfabdf8171adb52966
Inaamallah <inaamallah554@gmail.com>
2026-07-20 15:29:54 +0500

    Fix: CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: ['http://localhost:5173',"https://YOUR_FRONTEND.vercel.app"],
+    origin: ['http://localhost:5173',"https://novatech-ecomm-application.vercel.app"],
     credentials: true
 }))
 
