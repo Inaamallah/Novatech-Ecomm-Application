ab20ee4348ea9ce23369df5914fad30a749a8e16
Inaamallah <inaamallah554@gmail.com>
2026-07-20 15:11:18 +0500

    Fix: / ROuter

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,6 +16,9 @@ app.use(cors({
 
 app.use(express.json())
 app.use(cookieParser())
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 app.use('/api/auth', authRoutes)
 app.use('/api', dashboardRoutes)
