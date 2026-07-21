1a15021f9f74d4ec9760359f4a47f097f6f96b11
Inaamallah <inaamallah554@gmail.com>
2026-07-21 15:05:16 +0500

    Origin Change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: ['http://localhost:5173',"https://novatech-ecomm-application.vercel.app"],
+    origin: ['http://localhost:5173',"https://novastech.vercel.app"],
     credentials: true
 }))
 
